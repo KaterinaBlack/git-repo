1f4756c4e1ff1dc58a97cece8af7e5732f852b52
Mariya <mariya.petrova567@yandex.ru>
2016-05-07 16:23:33 +0300

    Добавлено приветствие

--- Test-program.cs
+++ Test-program.cs
@@ -5,6 +5,7 @@ class Program
 {
 public static void Main()
 {
+Console.WriteLine("Добрый день, товарищ Хейлсберг");
 Console.WriteLine("Ваше слово, товарищ Хейлсберг");
 Console.Write("Нажмите конопку для продолжения");
 Console.ReadKey(true);
