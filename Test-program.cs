764a6d86f1d6cf60f63e9a60eae4b6303fe88434
Mariya <mariya.petrova567@yandex.ru>
2016-05-07 16:19:45 +0300

    Создано консольное приложение для приветствия

---
+++ Test-program.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Lesson1
+{
+class Program
+{
+public static void Main()
+{
+Console.WriteLine("Ваше слово, товарищ Хейлсберг");
+Console.Write("Нажмите конопку для продолжения");
+Console.ReadKey(true);
+}
+}
+}
