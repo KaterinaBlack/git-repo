25bc7ba3d0365d9413542eeaecdf811ec4324be1
Mariya <mariya.petrova567@yandex.ru>
2016-06-03 14:32:58 +0300

    доработан класс приветствия. Добавлено приветствие друга

--- hi.cs
+++ hi.cs
@@ -6,6 +6,8 @@ class Program
 public static void Main()
 {
 Console.WriteLine("Hi");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello, my dear frend");
 Console.Write("Нажмите конопку для продолжения");
 Console.ReadKey(true);
 }
