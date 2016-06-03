84eb85ae3284243ada46d54ac4b5ecf03af3a46e
Mariya <mariya.petrova567@yandex.ru>
2016-06-03 23:22:36 +0300

    Добавлен новый класс Freedom

---
+++ Freedom.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Freedom
+{
+class Program
+{
+public static void Main()
+{
+Console.WriteLine("Freedom!!!");
+Console.Write("Нажмите конопку для продолжения");
+Console.ReadKey(true);
+}
+}
+}
