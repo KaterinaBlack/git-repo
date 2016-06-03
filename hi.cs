9cfa0f5cd4a6451a9b138d21092eb838371ce445
Mariya <mariya.petrova567@yandex.ru>
2016-06-03 14:41:41 +0300

    В класс приветствия добавлена фраза - рад тебя видеть

--- hi.cs
+++ hi.cs
@@ -8,6 +8,7 @@ public static void Main()
 Console.WriteLine("Hi");
 Console.WriteLine("Hello");
 Console.WriteLine("Hello, my dear frend");
+Console.WriteLine("Glad to see you");
 Console.Write("Нажмите конопку для продолжения");
 Console.ReadKey(true);
 }
