7ae43f56fa94b842cbfa1c07d2843b7191969f5d
Mariya <mariya.petrova567@yandex.ru>
2016-06-03 12:47:42 +0300

    Создан класс для прощания

---
+++ GoodBye.cs
@@ -0,0 +1,13 @@
+using System;
+namespace GoodBye
+{
+class Program
+{
+public static void Main()
+{
+Console.WriteLine("GoodBye");
+Console.Write("Нажмите конопку для продолжения");
+Console.ReadKey(true);
+}
+}
+}
