3fd4f9dbc9b3e4b4b8d221adaf79ad6328caf13c
Mariya <mariya.petrova567@yandex.ru>
2016-06-08 16:19:30 +0300

    Создан новый класс Love

---
+++ Love.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Love
+{
+class Program
+{
+public static void Main()
+{
+
+Console.WriteLine("I love you so much!!!;");
+Console.Write("Нажмите конопку для продолжения");
+Console.ReadKey(true);
+}
+}
+}
